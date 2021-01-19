34f29f6b84a4574a30327e42a87bbbcc7fa09fcc
MateuszPalewski <mateusz.palewski@o2.pl>
2021-01-19 20:48:31 +0100

    First commit

--- Sokoban/Form1.Designer.cs
+++ Sokoban/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Sokoban
 {
-    partial class Form1
+    partial class FormGame
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,42 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanelGame = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanelGame
+            // 
+            this.tableLayoutPanelGame.AutoSize = true;
+            this.tableLayoutPanelGame.ColumnCount = 2;
+            this.tableLayoutPanelGame.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanelGame.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanelGame.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanelGame.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanelGame.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanelGame.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanelGame.Name = "tableLayoutPanelGame";
+            this.tableLayoutPanelGame.RowCount = 2;
+            this.tableLayoutPanelGame.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanelGame.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanelGame.Size = new System.Drawing.Size(784, 761);
+            this.tableLayoutPanelGame.TabIndex = 0;
+            // 
+            // FormGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(784, 761);
+            this.Controls.Add(this.tableLayoutPanelGame);
+            this.Name = "FormGame";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanelGame;
     }
 }
 
--- Sokoban/Form1.cs
+++ Sokoban/Form1.cs
@@ -10,11 +10,45 @@ using System.Windows.Forms;
 
 namespace Sokoban
 {
-    public partial class Form1 : Form
+    public partial class FormGame : Form
     {
-        public Form1()
+        public FormGame()
         {
             InitializeComponent();
+
+            tableLayoutPanelGame.ColumnCount = 10;
+            tableLayoutPanelGame.RowCount = 10;
+            Panel[,] panels = new Panel[10,10];
+
+            Boolean boolColor = true;
+            for (int i = 0; i<10; i++)
+            {
+                
+                for(int j = 0; j<10; j++)
+                {
+                    panels[i, j] = new Panel();
+                    panels[i, j].Dock = DockStyle.Fill;
+                    panels[i, j].Margin = new Padding(0);
+                    panels[i, j].Padding = new Padding(0);
+                    boolColor = !boolColor;
+                    panels[i, j].BackColor = boolColor ? Color.Green : Color.Blue;
+                    tableLayoutPanelGame.Controls.Add(panels[i, j], i, j);
+
+                }
+                boolColor =! boolColor;
+            }
+
+            tableLayoutPanelGame.RowStyles.Clear();
+            tableLayoutPanelGame.ColumnStyles.Clear();
+            for (int i = 1; i <= tableLayoutPanelGame.RowCount; i++)
+            {
+                tableLayoutPanelGame.RowStyles.Add(new RowStyle(SizeType.Percent, 1));
+            }
+            for (int i = 1; i <= tableLayoutPanelGame.ColumnCount; i++)
+            {
+                tableLayoutPanelGame.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 1));
+            }
+
         }
     }
 }
